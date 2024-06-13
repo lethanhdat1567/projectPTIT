527ac0f6e61e612a4df89d792ac89e2f0b67ff4b
Le Thanh Dat <lethanhdat1567@gmail.com>
2024-06-13 15:14:38 +0700

    QLDH

--- MVC/views/pages/QLDH.php
+++ MVC/views/pages/QLDH.php
@@ -1,9 +1,9 @@
 <?php
-    $orderHistoryRows = [];
-    while ($row = mysqli_fetch_assoc($data["getOrderAdmin"])) {
-        $orderHistoryRows[] = $row;
-    }
- ?>
+$orderHistoryRows = [];
+while ($row = mysqli_fetch_assoc($data["getOrderAdmin"])) {
+    $orderHistoryRows[] = $row;
+}
+?>
 <div class="user-mana">
     <div class="container">
         <div class="admin-product__wrap">
@@ -34,6 +34,7 @@
                 <?php if (!empty($orderHistoryRows)): ?>
                     <?php 
                     $currentOrder = null;
+                    $total = 0; // Khởi tạo biến $total
                     foreach ($orderHistoryRows as $row): 
                         if ($currentOrder !== $row['order_date']): 
                             if ($currentOrder !== null): ?>
@@ -46,12 +47,18 @@
                             <?php 
                             endif; 
                             $currentOrder = $row['order_date'];
+                            $total = 0;
+                            foreach ($orderHistoryRows as $innerRow) {
+                                if ($innerRow['order_date'] == $currentOrder) {
+                                    $total += $innerRow['quantity'];
+                                }
+                            }
                             ?>
                             <div class="items-wrapper">
                                 <div class="orders__wrapper">
                                     <div class="orders__heading">
                                         <div class="orders__heading-info">
-                                            <h2 class="orders-info__user">Tên:<?php echo $row['fullname']; ?></h2>
+                                            <h2 class="orders-info__user">Tên: <?php echo $row['fullname']; ?> (Số lượng: <?php echo $total; ?>)</h2>
                                             <p class="orders-info__phone"><span class="orders__heading-item">Ngày đặt hàng: </span><?php echo $row['order_date']; ?></p>
                                             <p class="orders-info__phone"><span class="orders__heading-item">Số điện thoại: </span><?php echo $row['phone_number']; ?></p>
                                             <p class="orders-info__address"><span class="orders__heading-item">Địa chỉ: </span><?php echo $row['address']; ?></p>
@@ -61,16 +68,16 @@
                                     <div class="orders__items-wrapper">
                                         <div class="order__items">
                         <?php endif; ?>
-                                            <div class="order__item">
-                                                <img src="<?php echo ASSETS ?>img/products/<?php echo $row['thumbnail']; ?>" alt="" class="order__item-img" />
-                                                <div class="order__item-info">
-                                                    <h2 class="order__item-name"><?php echo $row['product_name']; ?></h2>
-                                                    <div class="order__item-quantity">Số lượng: <?php echo $row['quantity']; ?></div>
-                                                </div>
-                                                <div class="order__item-total">
-                                                    <p class="order__total-price"><?php echo number_format($row['order_details_total_money'], 0, ',', '.'); ?> đ</p>
-                                                </div>
-                                            </div>
+                            <div class="order__item">
+                                <img src="<?php echo ASSETS ?>img/products/<?php echo $row['thumbnail']; ?>" alt="" class="order__item-img" />
+                                    <div class="order__item-info">
+                                        <h2 class="order__item-name"><?php echo $row['product_name']; ?></h2>
+                                        <div class="order__item-quantity">Số lượng: <?php echo $row['quantity']; ?></div>
+                                    </div>
+                                <div class="order__item-total">
+                                    <p class="order__total-price"><?php echo number_format($row['order_details_total_money'], 0, ',', '.'); ?> đ</p>
+                                </div>
+                            </div>
                     <?php endforeach; ?>
                                         </div>
                                     </div>
